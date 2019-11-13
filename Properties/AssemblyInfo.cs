39eeb57dd57b7877572c2bf665fd4065dadb0226
sokooltools <ms@sokool.net>
2019-11-13 09:11:58 -0500

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DeleteTempFiles")]
+[assembly: AssemblyDescription("Deletes all files from %Temp% directory unless exclusions are specified as an argument in a shortcut.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SokoolTools")]
+[assembly: AssemblyProduct("DeleteTempFiles")]
+[assembly: AssemblyCopyright("Copyright © SokoolTools 2009-2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("339db3f8-f282-40aa-9b69-47c339818921")]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
---
+++ Readme.txt
@@ -0,0 +1,5 @@
+In your shortcut you can specify folders which should NOT be deleted by simply passing in a comma-delimited of folders as an argument.
+
+For example:
+
+"%ProgramFiles(x86)%\DevTools\DeleteTempFiles\DeleteTempFiles.exe" "JetBrains,ReSharperCache,SymbolCache,.com_ibm_tools_attach,notes256C9A"
