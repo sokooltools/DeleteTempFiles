ddc9e47537810148b3fce7cac954aa0516b350bc
SokoolTools <57449110+sokooltools@users.noreply.github.com>
2020-07-18 15:49:31 -0700

    Fixed issue with empty folder exclusion list causing exception; refactored code separating business logic from main.

---
+++ Processor.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace DeleteTempFiles
+{
+	public sealed class Processor
+	{
+		//------------------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Gets the number folders skipped.
+		/// </summary>
+		//------------------------------------------------------------------------------------------------------------------------
+		public int NumFoldersSkipped { get; private set; }
+
+		//------------------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Gets the number folders deleted.
+		/// </summary>
+		//------------------------------------------------------------------------------------------------------------------------
+		public int NumFoldersDeleted { get; private set; }
+
+		//------------------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Gets the number files deleted.
+		/// </summary>
+		//------------------------------------------------------------------------------------------------------------------------
+		public int NumFilesDeleted { get; private set; }
+
+		//------------------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Gets the number of items not deleted.
+		/// </summary>
+		//------------------------------------------------------------------------------------------------------------------------
+		public int NumItemsNotDeleted { get; private set; }
+
+		//------------------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// The list of all folders which will be excluded (skipped) during deletion.
+		/// </summary>
+		//------------------------------------------------------------------------------------------------------------------------
+		public List<string> ExcludedFolders { get; set; }
+
+		//------------------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Processor"/> class.
+		/// </summary>
+		//------------------------------------------------------------------------------------------------------------------------
+		public Processor()
+		{
+			ExcludedFolders = new List<string>();
+		}
+
+		//------------------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Recursively clears the specified folder of all its sub-folders and files.
+		/// </summary>
+		/// <param name="dirInfo">The directory info object.</param>
+		//------------------------------------------------------------------------------------------------------------------------
+		public void ClearFolder(DirectoryInfo dirInfo)
+		{
+			// First delete all the files in the folder.
+			foreach (FileInfo fiCurrFile in dirInfo.GetFiles())
+			{
+				try
+				{
+					Console.ResetColor();
+					Console.Write("Deleting File: {0}", TruncatePath(fiCurrFile.Name, 43).PadRight(43));
+					fiCurrFile.Delete();
+					//FileSystem.DeleteFile(fiCurrFile.FullName, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin, UICancelOption.ThrowException);
+					Console.ForegroundColor = ConsoleColor.Green;
+					Console.WriteLine(DELETED);
+					NumFilesDeleted++;
+				}
+				catch (Exception)
+				{
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine(COULD_NOT_DELETE);
+					Console.ResetColor();
+					NumItemsNotDeleted++;
+				}
+			}
+			Console.ResetColor();
+			foreach (DirectoryInfo diSubFolder in dirInfo.GetDirectories())
+			{
+				// Next delete each sub-folder in the folder.
+				if (ExcludedFolders.All(x => x != Path.GetFileName(diSubFolder.FullName)))
+				{
+					ClearFolder(diSubFolder); // Call recursively for all subfolders
+				}
+				try
+				{
+					Console.ResetColor();
+					Console.Write("Deleting SDir: {0}", TruncatePath(diSubFolder.FullName, 43).PadRight(43));
+
+					if (ExcludedFolders.All(x => x != Path.GetFileName(diSubFolder.FullName)))
+					{
+						diSubFolder.Delete(false);
+						//FileSystem.DeleteDirectory(diSubFolder.FullName, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin, UICancelOption.ThrowException);
+						Console.ForegroundColor = ConsoleColor.Green;
+						Console.WriteLine(DELETED);
+						NumFoldersDeleted++;
+					}
+					else
+					{
+						Console.ForegroundColor = ConsoleColor.DarkYellow;
+						Console.WriteLine(SKIPPED);
+						NumFoldersSkipped++;
+					}
+				}
+				catch (Exception)
+				{
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine(COULD_NOT_DELETE);
+					Console.ResetColor();
+					NumItemsNotDeleted++;
+				}
+			}
+		}
+
+		private const string SKIPPED = "            [Skipped]";
+		private const string DELETED = "            [Deleted]";
+		private const string COULD_NOT_DELETE = "   [Could not delete]";
+
+		[DllImport("shlwapi.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+		private static extern bool PathCompactPathEx(StringBuilder pszOut, string pszSrc, int cchMax, int dwFlags);
+
+		//------------------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Truncates the specified path if it exceeds the specified length.
+		/// </summary>
+		/// <param name="path">The path.</param>
+		/// <param name="maxLength">The length.</param>
+		/// <returns></returns>
+		//------------------------------------------------------------------------------------------------------------------------
+		private static string TruncatePath(string path, int maxLength)
+		{
+			var sb = new StringBuilder(260);
+			PathCompactPathEx(sb, path, maxLength + 1, 0);
+			return sb.ToString();
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,33 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
 
 namespace DeleteTempFiles
 {
 	internal static class Program
 	{
-		private static int _iFilesDeleted;
-		private static int _iFoldersSkipped;
-		private static int _iFoldersDeleted;
-		private static int _iFilesFoldersNotDeleted;
-
-		private const string SKIPPED = "            [Skipped]";
-		private const string DELETED = "            [Deleted]";
-		private const string COULD_NOT_DELETE = "   [Could not delete]";
-
-		[DllImport("shlwapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
-		private static extern bool PathCompactPathEx(StringBuilder pszOut, string pszSrc, int cchMax, int dwFlags);
-
-		//------------------------------------------------------------------------------------------------------------------------
-		/// <summary>
-		/// The list of all folders which will be excluded (skipped) during deletion.
-		/// </summary>
-		//------------------------------------------------------------------------------------------------------------------------
-		private static List<string> _excludedFolders;
-
 		//------------------------------------------------------------------------------------------------------------------------
 		/// <summary>
 		/// The main entry method along with any specified arguments.
@@ -36,6 +14,7 @@ namespace DeleteTempFiles
 		//------------------------------------------------------------------------------------------------------------------------
 		private static void Main(string[] args)
 		{
+			var processor = new Processor();
 			try
 			{
 				Console.Title = "Deleting Temp Files";
@@ -43,11 +22,9 @@ namespace DeleteTempFiles
 				Console.SetWindowSize(80, 40);
 
 				if (args.Length > 0)
-					_excludedFolders = args[0].Trim('"').Split(';', ',', '|').ToList();
+					processor.ExcludedFolders = args[0].Trim('"').Split(';', ',', '|').ToList();
 
 				// TODO: Read the excluded folder values from a file.
-
-				//string tempPath = Environment.GetFolderPath(Environment.SpecialFolder.InternetCache);
 
 				string tempPath = Environment.GetEnvironmentVariable("TEMP");
 				if (tempPath != null)
@@ -65,23 +42,23 @@ namespace DeleteTempFiles
 						DoExit();
 						return;
 					}
-					ClearFolder(new DirectoryInfo(tempPath)); // Execute ClearFolder() on the IE's cache folder
+					processor.ClearFolder(new DirectoryInfo(tempPath)); // Execute ClearFolder() on the IE's cache folder
 				}
 
 				Console.ForegroundColor = ConsoleColor.DarkYellow;
 				Console.WriteLine("");
-				Console.WriteLine("{0} folders skipped.", _iFoldersSkipped);
+				Console.WriteLine("{0} folders skipped.", processor.NumFoldersSkipped);
 
-				if (_iFilesFoldersNotDeleted > 0)
+				if (processor.NumItemsNotDeleted > 0)
 				{
 					Console.ForegroundColor = ConsoleColor.Red;
 					Console.WriteLine("");
-					Console.WriteLine("{0} folders and/or files could not be deleted.", _iFilesFoldersNotDeleted);
+					Console.WriteLine("{0} folders and/or files could not be deleted.", processor.NumItemsNotDeleted);
 				}
 
 				Console.ForegroundColor = ConsoleColor.Green;
 				Console.WriteLine("");
-				Console.WriteLine("{0} folders and/or files deleted.", _iFoldersDeleted + _iFilesDeleted);
+				Console.WriteLine("{0} folders and/or files deleted.", processor.NumFoldersDeleted + processor.NumFilesDeleted);
 			}
 			catch (Exception ex)
 			{
@@ -104,90 +81,6 @@ namespace DeleteTempFiles
 			Console.WriteLine("\n\nPress any key to continue...");
 			Console.ResetColor();
 			Console.ReadKey();
-		}
-
-		//------------------------------------------------------------------------------------------------------------------------
-		/// <summary>
-		/// Clears the folder.
-		/// </summary>
-		/// <param name="dirInfo">The directory info object.</param>
-		//------------------------------------------------------------------------------------------------------------------------
-		private static void ClearFolder(DirectoryInfo dirInfo)
-		{
-			Console.ResetColor();
-
-			// First delete all the files in the folder.
-			foreach (FileInfo fiCurrFile in dirInfo.GetFiles())
-			{
-				try
-				{
-					Console.ResetColor();
-					Console.Write("Deleting File: {0}", TruncatePath(fiCurrFile.Name, 43).PadRight(43));
-					fiCurrFile.Delete();
-					//FileSystem.DeleteFile(fiCurrFile.FullName, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin, UICancelOption.ThrowException);
-					Console.ForegroundColor = ConsoleColor.Green;
-					Console.WriteLine(DELETED);
-					_iFilesDeleted++;
-				}
-				catch (Exception)
-				{
-					Console.ForegroundColor = ConsoleColor.Red;
-					Console.WriteLine(COULD_NOT_DELETE);
-					Console.ResetColor();
-					_iFilesFoldersNotDeleted++;
-				}
-			}
-
-			foreach (DirectoryInfo diSubFolder in dirInfo.GetDirectories())
-			{
-				// Next delete each sub-folder in the folder.
-				if (_excludedFolders.All(x => x != Path.GetFileName(diSubFolder.FullName)))
-				{
-					ClearFolder(diSubFolder); // Call recursively for all subfolders
-				}
-				try
-				{
-					Console.ResetColor();
-					Console.Write("Deleting SDir: {0}", TruncatePath(diSubFolder.FullName, 43).PadRight(43));
-
-					if (_excludedFolders.All(x => x != Path.GetFileName(diSubFolder.FullName)))
-					{
-						diSubFolder.Delete(false);
-						//FileSystem.DeleteDirectory(diSubFolder.FullName, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin, UICancelOption.ThrowException);
-						Console.ForegroundColor = ConsoleColor.Green;
-						Console.WriteLine(DELETED);
-						_iFoldersDeleted++;
-					}
-					else
-					{
-						Console.ForegroundColor = ConsoleColor.DarkYellow;
-						Console.WriteLine(SKIPPED);
-						_iFoldersSkipped++;
-					}
-				}
-				catch (Exception)
-				{
-					Console.ForegroundColor = ConsoleColor.Red;
-					Console.WriteLine(COULD_NOT_DELETE);
-					Console.ResetColor();
-					_iFilesFoldersNotDeleted++;
-				}
-			}
-		}
-
-		//------------------------------------------------------------------------------------------------------------------------
-		/// <summary>
-		/// Truncates the specified path if it exceeds the specified length.
-		/// </summary>
-		/// <param name="path">The path.</param>
-		/// <param name="maxLength">The length.</param>
-		/// <returns></returns>
-		//------------------------------------------------------------------------------------------------------------------------
-		private static string TruncatePath(string path, int maxLength)
-		{
-			var sb = new StringBuilder(260);
-			PathCompactPathEx(sb, path, maxLength + 1, 0);
-			return sb.ToString();
 		}
 	}
 }
